eb4a61e7337ef2d5542962c8491491451da77a60
2002ANUJ <anujthukran123@gmail.com>
2023-06-29 01:44:56 +0530

    final commit

--- script.js
+++ script.js
@@ -37,18 +37,43 @@ const quizData = [
     }
 ]
 
+const quiz = document.getElementById('quiz');
+
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitbtn = document.getElementById('submit');
+const answerEls = document.querySelectorAll(".answer");
+
 let currentQuiz = 0;
+let score = 0;
+
+let answer = undefined;
 
 
 loadQuiz();
 
+
+function getSelected() {
+    
+   
+    let answer = undefined;
+
+    answerEls.forEach((answerEl)=>{
+        if(answerEl.checked){
+           answer = answerEl.id;
+        }
+    });
+
+    return answer;
+    
+}
+
+
 function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -60,28 +85,34 @@ function loadQuiz() {
 
 }
 
-function getSelected() {
-    console.log('hi');
-    const answers = document.querySelectorAll(".answer");
-
-    answers.forEach((answer)=>{
-        console.log(answer.value);
-    });
+function deselectAnswers() {
+    answerEls.forEach((answerEl)=>{
+       answerEl.checked = false;
+    })
 }
 
 
-submitbtn.addEventListener("click", ()=>{
+
+submitbtn.addEventListener("click", function(){
+   
+
+  const answer =  getSelected();
+  console.log(answer);
+
+  if(answer){
+    if(answer === quizData[currentQuiz].correct) {
+        score++;
+    }
     currentQuiz++;
+            if(currentQuiz<quizData.length){
+        
+                loadQuiz();
+            }else{
+                quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                <button onclick ="location.reload()">Reload</button>`
+            }
+
+  }
     
-  
-   console.log('on');
-   getSelected();
-    
-        if(currentQuiz<quizData.length){
-    
-            loadQuiz();
-        }else{
-            alert("you finished the quiz!")
-        }
 
 });
